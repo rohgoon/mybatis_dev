e1e1550e56418be9094e75320905d9440556bf0e
NCG <NCG@DESKTOP-T6ENAEQ>
2017-01-05 16:10:43 +0900

    dd

--- src/main/java/kr/co/dgit/bigdate/mybatis_dev/mappers/UserPicMapper.java
+++ src/main/java/kr/co/dgit/bigdate/mybatis_dev/mappers/UserPicMapper.java
@@ -3,6 +3,8 @@ package kr.co.dgit.bigdate.mybatis_dev.mappers;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Insert;
+
 import kr.co.dgit.bigdate.mybatis_dev.dto.Course;
 import kr.co.dgit.bigdate.mybatis_dev.dto.Student;
 import kr.co.dgit.bigdate.mybatis_dev.dto.UserPic;
@@ -10,5 +12,4 @@ import kr.co.dgit.bigdate.mybatis_dev.dto.UserPic;
 public interface UserPicMapper {
 	int insertUserPic(UserPic userPic);
 	UserPic getUserPic(int id);
-	
 }
